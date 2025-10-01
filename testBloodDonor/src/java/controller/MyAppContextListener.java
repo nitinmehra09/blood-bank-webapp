bc0b2fe7f3765dbe9fa3fb08f6024380fc6c633f
Nitin Mehra <145772092+nitinmehra09@users.noreply.github.com>
2025-10-01 21:57:34 +0530

    Remove unnecessary blank lines in MyAppContextListener

--- testBloodDonor/src/java/controller/MyAppContextListener.java
+++ testBloodDonor/src/java/controller/MyAppContextListener.java
@@ -3,11 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
 package controller;
-
 import com.mysql.cj.jdbc.AbandonedConnectionCleanupThread;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-
 public class MyAppContextListener implements ServletContextListener {
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
